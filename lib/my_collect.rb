0dd6734aa6bd344532617803c8f3af92b276684b
James Rogers <jarogers095@gmail.com>
2020-01-28 05:56:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    return_array = []
+    
+    while i < array.size do
+      return_array.push(yield(array[i]))
+      i += 1
+    end
+  end
+  return return_array
+end
 
