31a3f46d4b6b2e3875612076d9d0a9590976d196
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 19:59:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,32 @@
+def my_collect(empty_array)
+  i = 0 
+  collection = []
+  while i < empty_array.length 
+    collection << yield(array[i])
+    i += 1 
+  end
+end
 
+def my_collect(languages)
+  i = 0 
+  collection = []
+  while i < languages.length 
+    collection << yield(languages[i])
+    i += 1 
+  end
+  collection
+end
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) {|i| i.upcase}
+
+def my_collect(students)
+  i = 0 
+  collection = []
+  while i < students.length 
+    collection << yield(students[i])
+    i += 1 
+  end
+  collection
+end
+
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) {|i| i.split(" ").first}
