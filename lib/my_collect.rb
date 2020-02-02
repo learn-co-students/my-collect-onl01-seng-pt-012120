9ef2b9ef1d29825856551c9e0ca5640382bb0eba
Hailey P <heyputnam@gmail.com>
2020-02-02 22:07:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
 
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i += 1
+    end
+    collect
+end
+
+my_collect(languages) do | language |
+  language.upcase
+end
