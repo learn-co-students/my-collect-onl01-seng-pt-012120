8a44814fc14bf7e134040c276113a1abad0a29be
Patricia C <patricia@patriciaco.com>
2020-02-03 02:19:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
+end
 
+array = ["Mickey Mouse", "Daisy", "Donald Duck"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
