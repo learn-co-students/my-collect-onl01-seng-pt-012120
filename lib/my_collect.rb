420f85af4160c2fab0bf1de3637e349c7109bade
jonathan e godinez <jongodinezbass@yahoo.com>
2020-02-03 01:34:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(empty_array)
+  counter =0 
+  names = [] 
+  while counter < empty_array.length 
+  names << yield(empty_array [counter])       
+         counter += 1
+       end
+       names
+end
